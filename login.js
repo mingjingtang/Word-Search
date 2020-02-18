c7195bf7b8973e8e58abba4e1133d0d8aeeb4154
mingjing tang <tmjcsh@gmail.com>
2020-02-18 15:58:01 -0500

    add keyup to login

--- login.js
+++ login.js
@@ -1,7 +1,14 @@
-
 let submitButton = document.getElementById("arrow");
+let input = document.getElementById("name");
 
-submitButton.addEventListener("click", function(){
-    let name = document.getElementById('name').value;
-    localStorage.setItem( 'playerName', name);
+submitButton.addEventListener("click", function() {
+  let name = document.getElementById("name").value;
+  localStorage.setItem("playerName", name);
+});
+
+input.addEventListener("keyup", function(event) {
+  event.preventDefault();
+  if (event.keyCode === 13) {
+    submitButton.click();
+  }
 });
