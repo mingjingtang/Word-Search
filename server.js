2f2ab530b16cbdf959ff16a748713bd0b762d216
mingjing tang <tmjcsh@gmail.com>
2020-05-26 16:40:32 -0400

    edit server.js file delete build related path

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require("express");
 const favicon = require("express-favicon");
 const path = require("path");
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 5500;
 const app = express();
 
 // the __dirname is the current directory from where the script is running
-app.use(favicon(__dirname + "/build/favicon.ico"));
+// app.use(favicon(__dirname + "/build/favicon.ico"));
 
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, "build")));
-app.get("/ping", function (req, res) {
-  return res.send("pong");
-});
+// app.use(express.static(path.join(__dirname, "build")));
+
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, index.html));
 });
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
